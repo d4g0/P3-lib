3fa3745d9c0b58c0b0e41eda577d90ab747f8e60
d4g0 <tocarralero@gmail.com>
2021-12-16 08:45:15 -0500

    tested

---
+++ sorting/binarySearch.mjs
@@ -0,0 +1,27 @@
+/** 
+ * BinarySearch for Arrays
+ * @param {*} arr 
+ * @param {*} key 
+ */
+export function binarySearch(arr, key) {
+    var min = 0;
+    var max = arr.length - 1;
+    var half = 0;
+    var result = -1;
+
+    while (min < max) {
+        half = Math.floor((min + max) / 2);
+        if (arr[half] == key) {
+            result = half;
+            return result;
+        }
+        if (arr[half] < key) {
+            min = half + 1;
+        }
+        else {
+            max = half - 1;
+        }
+    }
+    return result;
+}
+
